66ee984d4a95951d4cd2030f98ccbe7605d25c3c
monikakalt <kaltenytmonika3@gmail.com>
2018-04-11 10:15:57 +0300

    added navbar component

--- challengeAngular/src/app/app.module.ts
+++ challengeAngular/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './shared/app.routing';
+import { NavComponent } from './shared/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
