64f98c96ae41621c788c2eb924ab59adeef72dfc
JasonasIgn <ignas.jasonas@gmail.com>
2018-04-17 15:44:22 +0300

    added navbar, footer components, some homepage content

--- challengeAngular/src/app/app.module.ts
+++ challengeAngular/src/app/app.module.ts
@@ -5,21 +5,23 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './shared/app.routing';
-import { NavComponent } from './shared/navbar.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ChallengeComponent } from './challenge/challenge.component';
 import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavComponent,
     LoginComponent,
     RegisterComponent,
     ChallengeComponent,
-    AboutComponent
+    AboutComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
